d5ccb4ccebfdf7225eb0e7eb7f15821beb1a0917
zaalie <zaaalie24@gmail.com>
2026-06-06 23:27:10 +0700

    commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,15 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+console.log('DATABASE_URL =', process.env.DATABASE_URL);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
